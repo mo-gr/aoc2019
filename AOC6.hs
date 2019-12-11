979c39a192285e9c9bcad7ec53538ad00654e484
Moritz Grauel <mgrauel@atlassian.com>
2019-12-11 12:13:13 +1100

    Day 6 part 2

--- AOC6.hs
+++ AOC6.hs
@@ -20,8 +20,9 @@ import qualified Hedgehog.Range as Range
 import Data.Functor (($>))
 import Data.List (find)
 import Data.Maybe (fromMaybe)
+import Data.Set (Set, fromList, intersection, notMember)
 
-data Planet = Planet { name :: String } deriving (Eq, Show)
+data Planet = Planet { name :: String } deriving (Eq, Show, Ord)
 data Orbit = Orbit { center :: Planet, outer :: Planet } deriving (Eq, Show)
 type WeightedOrbit = (Orbit, Int)
 
@@ -59,8 +60,23 @@ calculateDistanceToCom os o = fromMaybe (o,1) $ do
     let pw = snd $ calculateDistanceToCom os po
     return (o, 1 + pw)
 
+allParents :: [Orbit] -> Planet -> [Planet]
+allParents os = allParents' os os where
+    allParents' _ [] _ = []
+    allParents' os (o:_) p | outer o == p = (center o) : allParents' os os (center o)
+    allParents' os (o:os') p = allParents' os os' p
+
 orbitSum :: [WeightedOrbit] -> Int
 orbitSum = sum . map snd
+
+transferCount :: [WeightedOrbit] -> Planet -> Planet -> Int
+transferCount os p1 p2= let
+    orbits = fst <$> os
+    p1Parents = allParents orbits p1
+    p2Parents = allParents orbits p2
+    commonParents = intersection (fromList p1Parents) (fromList p2Parents)
+    withoutCommons = filter (\p -> notMember p commonParents)
+    in length (withoutCommons p1Parents) + length (withoutCommons p2Parents)
 
 -- 194721
 solution1 :: IO Int
@@ -70,8 +86,15 @@ solution1 = do
         Right orbs' -> return $ orbitSum $ map (calculateDistanceToCom orbs') orbs'
         Left e -> error (show e)
 
+-- 316
 solution2 :: IO Int
-solution2 = error "no solution yet"
+solution2 = do
+    orbs <- parseFromFile inputParser "AOC6.input"
+    let you = Planet "YOU"
+        santa = Planet "SAN"
+    case orbs of
+        Right orbs' -> return $ transferCount (map (calculateDistanceToCom orbs') orbs') you santa
+        Left e -> error (show e)
 
 -- TESTS
 
@@ -89,11 +112,33 @@ prop_example =
         Right x -> (snd <$> map (calculateDistanceToCom x) x) H.=== [1 , 2 , 3 , 4 , 5 , 2 , 3 , 4 , 5 , 6 , 7]
         Left e -> H.footnote (show e) >> H.failure 
 
+prop_transfer :: H.Property
+prop_transfer =
+    let you = Planet "YOU"
+        santa = Planet "SAN"
+    in
+    H.withTests 1 $ H.property $
+      case parse inputParser "example_transfer" transfer_example of
+        Right x -> (transferCount (map (calculateDistanceToCom x) x) you santa) H.=== 4
+        Left e -> H.footnote (show e) >> H.failure 
+
+prop_parents :: H.Property
+prop_parents =
+    let you = Planet "YOU" in
+    H.withTests 1 $ H.property $
+      case parse inputParser "example_transfer" transfer_example of
+        Right x -> (name <$> allParents (fst <$> map (calculateDistanceToCom x) x) you) H.=== ["K", "J", "E", "D", "C", "B", "COM"]
+        Left e -> H.footnote (show e) >> H.failure 
+
 example = "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L" 
+
+transfer_example = "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L K)YOU I)SAN"
 
 tests :: IO Bool
 tests = H.checkParallel $ H.Group "AOC5" [
         ("prop_parser", prop_parser),
-        ("prop_example", prop_example)
+        ("prop_example", prop_example),
+        ("prop_transfer", prop_transfer),
+        ("prop_parents", prop_parents)
     ]
         
