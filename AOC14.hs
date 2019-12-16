b927d7ebb9d139ebed45646c9465ea6cde4339ca
Moritz Grauel <mgrauel@atlassian.com>
2019-12-16 18:30:37 +1100

    Brute Day 16

--- AOC14.hs
+++ AOC14.hs
@@ -4,8 +4,15 @@ module AOC14
     )
 where
 
+import           Control.Monad                  ( guard )
+import           Debug.Trace                    ( trace )
+import           Data.List                      ( sort )
+
 solution1 :: IO ()
 solution1 = error "no solution yet"
 
 solution2 :: IO ()
 solution2 = error "no solution yet"
+
+data Chemical = Chemical {name :: String} deriving (Eq, Show, Ord)
+
